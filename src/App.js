3e7111133bc6e60703fbba0c7aae8bee53f4ee59
Magda Jarzyna <magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.comgit config --global user.email magdajarzyna@gmail.com>
2021-06-07 12:08:02 +0200

    tam

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React app cos
+          React app cos tam
         </p>
         <a
           className="App-link"
